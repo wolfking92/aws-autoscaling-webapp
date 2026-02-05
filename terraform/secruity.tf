ee01faaa1201db7aea1a29d851aa46696eb1892b
wolfking92 <halfbloodprince9253@gmail.com>
2026-02-05 17:57:31 +0530

    Update secruity.tf

--- terraform/secruity.tf
+++ terraform/secruity.tf
@@ -9,13 +9,13 @@ resource "aws_security_group" "web_sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    ingress {
-
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks =["0.0.0.0/0"]
-    }
+ #  ingress {
+ #
+ #     from_port = 80
+ #    to_port = 80
+ #   protocol = "tcp"
+ #  cidr_blocks =["0.0.0.0/0"] 
+ #  }
     
 
     egress {
